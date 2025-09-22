8390c773037fd9706e1358a35c8f825e3f9db0b1
Sintja <sporeci2000@gmail.com>
2025-09-21 21:07:39 -0400

    fixing

--- server.js
+++ server.js
@@ -22,7 +22,14 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000", // local dev
+    "https://pro-tasker-frontend-5wc9.onrender.com" // Render frontend
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
